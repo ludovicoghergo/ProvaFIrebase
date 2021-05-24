fa1b1f809d5b1d8425e0c9810f799a1730903cfe
mattia zito <mattia.zito@edu.unito.it>
2021-05-24 11:05:34 +0200

    aggiunta account activity

--- app/src/main/java/com/example/provafirebase/AccountActivity.kt
+++ app/src/main/java/com/example/provafirebase/AccountActivity.kt
@@ -1,11 +1,34 @@
 package com.example.provafirebase
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class AccountActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account)
+
+        val name: TextView = findViewById(R.id.textView5)
+        name.text = SavedPreference.getUsername(this)
+
+        val eMail: TextView = findViewById(R.id.textView7)
+        eMail.text = SavedPreference.getEmail(this)
+
+        val actionbar = supportActionBar
+        //set actionbar title
+        actionbar!!.title = "Account page"
+        //set back button
+        actionbar.setDisplayHomeAsUpEnabled(true)
+        actionbar.setDisplayHomeAsUpEnabled(true)
+
+    }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
--- app/src/main/java/com/example/provafirebase/GroupsActivity.kt
+++ app/src/main/java/com/example/provafirebase/GroupsActivity.kt
@@ -2,10 +2,13 @@ package com.example.provafirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class GroupsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_groups)
+
+
     }
 }
